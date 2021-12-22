89d1b3c6668531093b50a52e3a61b28d4d67a875
Justin Conner <justin@americanhunt.com>
2021-12-22 16:12:41 -0500

    Update meta again

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ import holdingFishImage from "../public/holding-fish.jpg";
 const META = {
   title: "Knot@Work Fishing Charters",
   description: "Fishing charters for your family and friends off the coast of Virginia and North Carolina.",
-  type: "business.business",
-  image: holdingFishImage,
-  url: "https://www.knotatworkcharters.com",
+  type: "website",
+  image: `https://knotatworkcharters.com${holdingFishImage.src}`,
+  url: "https://knotatworkcharters.com",
 };
 
 export default function Home() {
@@ -19,7 +19,9 @@ export default function Home() {
         <meta name="description" content={META.description} />
         <link rel="icon" href="/favicon.ico" />
 
-        <meta property="og:title" content={META.title} />
+        <link rel="canonical" href={META.url} />
+
+        <meta property="og:title" content="Fishing Charters in Virginia and North Carolina." />
         <meta property="og:type" content={META.type} />
         <meta property="og:description" content={META.description} />
         <meta property="og:image" content={META.image} />
@@ -28,6 +30,10 @@ export default function Home() {
 
         <meta property="og:site_name" content={META.title} />
         <meta name="twitter:image:alt" content="Alt text for image" />
+
+        <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+        <meta name="robots" content="index, follow" />
+        <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
@@ -38,8 +44,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <video className={styles.videoContainer} autoPlay muted loop id="myVideo">
-          <source src="https://cdn.veed.io/render/35e4f8be-d048-4eef-ad79-06f2f19bbf66.mp4" type="video/mp4" />
+        <video className={styles.videoContainer} autoPlay muted loop>
+          <source src="background.mp4" type="video/mp4" />
         </video>
         <MainContent />
       </main>
